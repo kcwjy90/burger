2b4b64a40541c6c9379a954f83aaa1dbc207bf98
kcwjy90 <kcwjy90@gmail.com>
2018-11-17 23:06:46 -0800

    please

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,3 +1,51 @@
+
+
+$(function() {
+  $(".change").on("click", function(event) {
+    var id = $(this).data("id");
+    var newEat = $(this).data("eating");
+    console.log(id, newEat);
+
+    var newateState = {
+      ate: newEat
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newateState
+    }).then(
+      function() {
+        console.log("changed ate to", newateState);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#bu").val().trim(),
+      ate: true
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers/", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
+
 // // Make sure we wait to attach our handlers until the DOM is fully loaded.
 
 // $(function () {
@@ -66,52 +114,3 @@
 
 //   });
 // });
-
-
-
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change").on("click", function(event) {
-    var id = $(this).data("id");
-    var newEat = $(this).data("eating");
-    console.log(id, newEat);
-
-    var newateState = {
-      ate: newEat
-    };
-
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newateState
-    }).then(
-      function() {
-        console.log("changed ate to", newateState);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-
-    var newBurger = {
-      name: $("#bu").val().trim(),
-      ate: true
-    };
-
-    // Send the POST request.
-    $.ajax("/api/burgers/", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-});

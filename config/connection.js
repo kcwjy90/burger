d2fdfa227a9a131b126d533c6b59f59423c122dc
kcwjy90 <kcwjy90@gmail.com>
2018-11-20 18:08:37 -0800

    ha

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 //setting up mysql connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+
+    connection = mysql.createConnection({
     hots: "localhost",
     user: "root",
     password: "",
     database: "burgers_db"
-})
-
+});
+};
 
 //Making connection
 connection.connect(function(err){
